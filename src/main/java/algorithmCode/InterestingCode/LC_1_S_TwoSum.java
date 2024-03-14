9d3fdc2314993fd40f73b278d8de3a53908261da
guwanli <huajianzhiyue@sina.com>
2024-03-14 10:28:32 +0800

    1. 两数之和

--- src/main/java/algorithmCode/InterestingCode/LC_1_S_TwoSum.java
+++ src/main/java/algorithmCode/InterestingCode/LC_1_S_TwoSum.java
@@ -11,13 +11,16 @@ import java.util.Map;
  */
 public class LC_1_S_TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        //利用map存储可能结果
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int temp = target - nums[i];
+            //判断map中有没有需要的值
             if (map.containsKey(temp)) {
                 int[] solution = {map.get(temp), i};
                 return solution;
             }
+            //把元素放到map中去
             map.put(nums[i], i);
         }
         return null;
