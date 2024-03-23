1cec176fb2025f2b61f55a56c57399ecc4b626e5
bc <huajianzhiyue@sina.com>
2024-03-23 11:53:21 +0800

    2549.统计桌面上的不同数字

---
+++ src/main/java/algorithmCode/InterestingCode/LC_2549_S_distinctIntegers.java
@@ -0,0 +1,14 @@
+package algorithmCode.InterestingCode;
+
+/**
+ * 2549.统计桌面上的不同数字
+ *
+ * @Author bc
+ * @Date 2024/3/23 11:52
+ * @Description:
+ */
+public class LC_2549_S_distinctIntegers {
+    public int distinctIntegers(int n) {
+        return n == 1 ? 1 : n - 1;
+    }
+}
