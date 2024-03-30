af9b8373ad16fd69c1a15a8612ebd2953324a183
bc <huajianzhiyue@sina.com>
2024-03-31 00:25:46 +0800

    1. 两数之和

--- src/main/java/algorithmCode/InterestingCode/LC_1_S_TwoSum.java
+++ src/main/java/algorithmCode/InterestingCode/LC_1_S_TwoSum.java
@@ -10,17 +10,24 @@ import java.util.Map;
  * @Date 2023/3/11 11:40
  */
 public class LC_1_S_TwoSum {
+    /**
+     * 查找数组中两个数的索引，使得它们的和等于一个特定目标数。
+     *
+     * @param nums 包含整数的数组。
+     * @param target 目标和。
+     * @return 包含两个整数索引的数组，使得 nums[index1] + nums[index2] = target。如果找不到这样的两个数，则返回 null。
+     */
     public int[] twoSum(int[] nums, int target) {
-        //利用map存储可能结果
+        // 使用map存储已遍历的数字及其索引
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int temp = target - nums[i];
-            //判断map中有没有需要的值
+            // 检查map中是否存在目标数
             if (map.containsKey(temp)) {
                 int[] solution = {map.get(temp), i};
                 return solution;
             }
-            //把元素放到map中去
+            // 将当前数字及其索引加入map
             map.put(nums[i], i);
         }
         return null;
