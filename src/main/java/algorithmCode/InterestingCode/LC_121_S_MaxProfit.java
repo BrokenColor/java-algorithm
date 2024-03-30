cbd20532eb5747968a5d284f2de8e5c805841548
bc <huajianzhiyue@sina.com>
2024-03-31 00:30:36 +0800

    121.买卖股票的最佳时机

--- src/main/java/algorithmCode/InterestingCode/LC_121_S_MaxProfit.java
+++ src/main/java/algorithmCode/InterestingCode/LC_121_S_MaxProfit.java
@@ -8,21 +8,28 @@ package algorithmCode.InterestingCode;
  */
 public class LC_121_S_MaxProfit {
     public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4};
         LC_121_S_MaxProfit solution = new LC_121_S_MaxProfit();
-//        System.out.println(solution.maxProfit(prices));
-//        System.out.println(solution.maxProfit1(prices));
-        System.out.println(solution.maxProfit2(prices));
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(solution.maxProfit1(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(solution.maxProfit2(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
+    /**
+     * 计算在给定股票价格数组中能够实现的最大利润。
+     *
+     * @param prices 股票每天的价格数组。
+     * @return 返回能够实现的最大利润。
+     */
     public int maxProfit(int[] prices) {
+        // 初始化卖出利润为0，买入利润为最小整数，确保第一次买入时是最低价。
         int sell = 0, buy = Integer.MIN_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            //卖出的利润=  max（不操作的利润，卖出股票的利润）
+            // 根据当前价格，更新卖出利润，选择不卖出或卖出当前股票的利润中的最大值。
             sell = Math.max(sell, buy + prices[i]);
-            //买入的利润=  max（不操作的利润，买入股票的利润）
+            // 根据当前价格，更新买入利润，选择不买入或买入当前股票的利润中的最大值。
             buy = Math.max(buy, -prices[i]);
         }
+        // 返回最后的卖出利润，即为整个过程中的最大利润。
         return sell;
     }
 
