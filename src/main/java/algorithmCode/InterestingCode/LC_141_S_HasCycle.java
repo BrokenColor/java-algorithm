6d1924ea52a894eb3d4c9a30ffa553d8b4be3e97
bc <huajianzhiyue@sina.com>
2024-04-07 23:31:48 +0800

    141.环形链表

--- src/main/java/algorithmCode/InterestingCode/LC_141_S_HasCycle.java
+++ src/main/java/algorithmCode/InterestingCode/LC_141_S_HasCycle.java
@@ -5,7 +5,7 @@ import algorithmCode.InterestingCode.entity.ListNode;
 /**
  * 141.环形链表
  *
- * @author guwanli
+ * @author bc
  * @date 2022/1/5 17:29
  */
 public class LC_141_S_HasCycle {
@@ -23,23 +23,34 @@ public class LC_141_S_HasCycle {
     }
 
     /**
-     * 快慢指针
+     * 检查给定的链表是否包含环。
+     * 使用快慢指针的方法，快指针每次移动两步，慢指针每次移动一步。
+     * 如果链表中存在环，那么快慢指针最终会相遇；如果没有环，则快指针会先到达链表尾部。
      *
-     * @param head
-     * @return
+     * @param head 链表的头节点。类型为ListNode，表示链表的起始节点。
+     * @return 返回一个布尔值，如果链表中存在环则返回true，否则返回false。
      */
     public boolean hasCycle(ListNode head) {
+        // 链表为空或只有一个节点时不可能存在环
         if (head == null || head.next == null) return false;
-        //定义两个指针
+
+        // 初始化快慢指针
         ListNode fast = head.next, low = head;
+
+        // 使用快慢指针遍历链表，检查是否存在环
         while (fast != low) {
-            //快指针走完都没有重合说明没有环
+            // 如果快指针或其下一个节点为空，则链表无环
             if (fast == null || fast.next == null) return false;
+
+            // 快指针每次移动两步，慢指针每次移动一步
             fast = fast.next.next;
             low = low.next;
         }
+
+        // 若快慢指针相遇，则链表存在环
         return true;
     }
+
 
     public boolean hasCycle1(ListNode head) {
         ListNode fast = head, low = head;
