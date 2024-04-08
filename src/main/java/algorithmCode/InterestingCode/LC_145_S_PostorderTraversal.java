2ce05da7ae1fb919559df4f06290ec0e80668eb3
bc <huajianzhiyue@sina.com>
2024-04-08 23:55:11 +0800

    145. 二叉树的后序遍历

--- src/main/java/algorithmCode/InterestingCode/LC_145_S_PostorderTraversal.java
+++ src/main/java/algorithmCode/InterestingCode/LC_145_S_PostorderTraversal.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * 145. 二叉树的后序遍历
  *
- * @author guwanli
+ * @author bc
  * @date 2021/12/7 09:59
  */
 public class LC_145_S_PostorderTraversal {
@@ -26,11 +26,20 @@ public class LC_145_S_PostorderTraversal {
         return res;
     }
 
-    //递归方式处理
+    /**
+     * 以递归方式对二叉树进行中序遍历，将遍历结果存储到一个列表中。
+     *
+     * @param root 二叉树的根节点
+     * @param res 存储遍历结果的列表
+     */
     private void inorder(TreeNode root, List<Integer> res) {
+        // 如果当前节点为空，直接返回
         if (root == null) return;
+        // 遍历左子树
         inorder(root.left, res);
+        // 遍历右子树
         inorder(root.right, res);
+        // 将当前节点的值添加到结果列表中
         res.add(root.val);
     }
 }
