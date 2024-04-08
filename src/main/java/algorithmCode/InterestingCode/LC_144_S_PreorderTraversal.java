531cf62d9b10b67bc63dbefa6fb18e3061cbe2e1
bc <huajianzhiyue@sina.com>
2024-04-08 23:54:24 +0800

    144. 二叉树的前序遍历

--- src/main/java/algorithmCode/InterestingCode/LC_144_S_PreorderTraversal.java
+++ src/main/java/algorithmCode/InterestingCode/LC_144_S_PreorderTraversal.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * 144. 二叉树的前序遍历
  *
- * @author guwanli
+ * @author bc
  * @date 2021/12/7 09:59
  */
 public class LC_144_S_PreorderTraversal {
@@ -26,11 +26,18 @@ public class LC_144_S_PreorderTraversal {
         return res;
     }
 
-    //递归方式处理
+    /**
+     * 中序遍历二叉树的递归实现。
+     *
+     * @param root 二叉树的当前节点，如果为null表示遍历到叶子节点了。
+     * @param res 存放遍历结果的列表，遍历过程中会将节点的值添加到这个列表中。
+     */
     private void inorder(TreeNode root, List<Integer> res) {
+        // 当前节点为空，直接返回，这是递归的终止条件
         if (root == null) return;
-        res.add(root.val);
-        inorder(root.left, res);
-        inorder(root.right, res);
+        // 遍历左子树
+        res.add(root.val); // 将当前节点的值添加到结果列表中
+        inorder(root.left, res); // 递归遍历左子树
+        inorder(root.right, res); // 递归遍历右子树
     }
 }
