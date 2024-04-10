77b0300d31dfd4e42f39deabe4c40731611b1ade
bc <huajianzhiyue@sina.com>
2024-04-11 00:00:38 +0800

    160. 相交链表

--- src/main/java/algorithmCode/InterestingCode/LC_160_S_GetIntersectionNode.java
+++ src/main/java/algorithmCode/InterestingCode/LC_160_S_GetIntersectionNode.java
@@ -5,7 +5,7 @@ import algorithmCode.InterestingCode.entity.ListNode;
 /**
  * 160. 相交链表
  *
- * @author guwanli
+ * @author bc
  * @date 2022/1/5 19:51
  */
 public class LC_160_S_GetIntersectionNode {
@@ -29,33 +29,41 @@ public class LC_160_S_GetIntersectionNode {
         System.out.println(solution.getIntersectionNode(headA, headB).val);
     }
 
+    /**
+     * 获取两个链表的交点
+     * @param headA 第一个链表的头结点
+     * @param headB 第二个链表的头结点
+     * @return 两个链表的交点；如果没有交点，返回null。
+     */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) return null;
-        //先判断链表是否有交集
+        // 先判断链表是否有交集
         ListNode pA = headA, pB = headB;
         while (pA.next != null || pB.next != null) {
             if (pA.next != null) pA = pA.next;
             if (pB.next != null) pB = pB.next;
         }
+        // 如果遍历结束pA和pB不相等，则链表没有交集
         if (pA != pB) {
             return null;
         }
-        // p1 指向 A 链表头结点，p2 指向 B 链表头结点
+
+        // 使用双指针法找到交点
         ListNode p1 = headA, p2 = headB;
         while (p1 != p2) {
-            // p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            // p1遍历完A链表后转到B链表
             if (p1.next != null) {
                 p1 = p1.next;
             } else {
                 p1 = headB;
             }
-            // p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            // p2遍历完B链表后转到A链表
             if (p2.next != null) {
                 p2 = p2.next;
             } else {
                 p2 = headA;
             }
         }
+        // 返回交点结点
         return p1;
     }
 }
